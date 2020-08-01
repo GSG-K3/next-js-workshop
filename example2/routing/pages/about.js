125ffb6d8bf23c44c15f62d709253649714cc88c
Nujood <shawar.nujood@gmail.com>
2020-08-02 02:13:15 +0300

    adding home & about page with their layout component for Links

---
+++ example2/routing/pages/about.js
@@ -0,0 +1,10 @@
+import Layout from "../components/layouts/layout";
+
+const About = () => (
+  <div>
+    <Layout>
+      <h1>This is about </h1>
+    </Layout>
+  </div>
+);
+export default About;
---
+++ example2/routing/pages/index.js
@@ -0,0 +1,12 @@
+// import styles from '../styles/Home.module.css'
+import Layout from "../components/layouts/layout";
+
+export default function Home() {
+  return (
+    <div>
+      <Layout>
+        <h1>This is Home Orangeeee </h1>
+      </Layout>
+    </div>
+  );
+}
