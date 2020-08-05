70684b3b5afa28db970d30b373bea8b78eb47318
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-08-05 20:08:43 +0200

    add the content of home page in dynamoc routing example

--- example/dynamic-routing/pages/index.js
+++ example/dynamic-routing/pages/index.js
@@ -1,1 +1,20 @@
-export default () => <h1>Home</h1>;
+import data from '../assets/data';
+
+export default function Home() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+      }}
+    >
+      {data.map((meal) => (
+        <div key={meal.id}>
+          {/* add your link here */}
+          <img src={meal.imageUrl} />
+        </div>
+      ))}
+    </div>
+  );
+}
