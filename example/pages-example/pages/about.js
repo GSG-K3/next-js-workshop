fee0bab7adf35a339f576c68ac94e2b139484def
bayan-404 <bayan.jubah@gmail.com>
2020-08-05 21:56:50 +0300

    just some files for the example about pages

---
+++ example/pages-example/pages/about.js
@@ -0,0 +1,7 @@
+
+const About = () =>{
+ return (
+    <h1>hello it's about page</h1>
+  )
+}
+export default About
---
+++ example/pages-example/pages/index.js
@@ -0,0 +1,6 @@
+const HomePage =()=>{
+return(
+  <h1>hello it's me </h1>
+)
+}
+export default HomePage
