4279b14003f90e8ded54d338be37bb06d98ddc34
bayan-404 <bayan.jubah@gmail.com>
2020-08-05 22:13:11 +0300

    these are the files the studets will add following the example so we dont need them

--- example/pages-example/pages/about.js
+++
@@ -1,7 +0,0 @@
-
-const About = () =>{
- return (
-    <h1>hello it's about page</h1>
-  )
-}
-export default About
--- example/pages-example/pages/index.js
+++
@@ -1,6 +0,0 @@
-const HomePage =()=>{
-return(
-  <h1>hello it's me </h1>
-)
-}
-export default HomePage
