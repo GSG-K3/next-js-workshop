79621b830a3ca0525220701e0818c49802ebd322
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-08-06 13:39:38 +0200

    remove project folder

--- example/project/pages/about.jsx
+++
@@ -1,8 +0,0 @@
-import Layout from '../components/Layout';
-export default () => (
-  <Layout keywords="about page KW " description="about page Desc">
-    <div>
-      <h1>About us</h1>
-    </div>
-  </Layout>
-);
